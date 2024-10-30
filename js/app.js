8f1a0da9844807c753b40c1e6e21bbb348dc3f2d
Angela Torro <a.torroballester@gmail.com>
2024-10-30 21:48:58 +0100

    Revert "Add user registration and login functionality with validation"
    This reverts commit ec3a52cf8ec850a51e0746c5c1287661f0ad1a43.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
         '/about': '../js/views/about.view.html',
         '/contact': '../js/views/contact.view.html',
         '/login': '../js/views/login.view.html',
-        '/register': '../js/views/register.view.html',
         '/cart': '../js/views/cart.view.html',
     };
 
@@ -21,8 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 contentDiv.innerHTML = html;
 
                 if (route === '/shop') {
-                    initShop();
-                    document.getElementById('go-back').addEventListener('click', goBack); 
+                    initShop(); // Iniciar la tienda
+                    document.getElementById('go-back').addEventListener('click', goBack); // Añadir manejador para el botón de regresar
                 }
             })
             .catch(error => console.error('Error al cargar la página:', error));
